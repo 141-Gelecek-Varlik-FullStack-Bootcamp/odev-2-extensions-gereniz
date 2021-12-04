d3117d3c082b934df74dd19614214e328454a493
gereniz <gustagereniz03@gmail.com>
2021-12-04 14:09:44 +0300

    1

---
+++ odev-2-extensions-gereniz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace odev_2_extensions_gereniz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
